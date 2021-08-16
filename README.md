7c2f110ae85ba71cf4ed73f4c400382967bf96a1
Sérgio Silveira <sdsilveira@gmail.com>
2021-08-16 12:29:56 +0200

    updated packages and readme

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# Resistance.Postgres #
+# Resistance.Postgres
+
+[![MIT License](https://img.shields.io/github/license/RagingKore/resistance.postgres)](LICENSE)
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/RagingKore/resistance.postgres/Publish%20NuGet)
+[![Nuget](https://img.shields.io/nuget/v/resistance.postgres.svg)](https://www.nuget.org/packages/resistance.postgres/)
+
 
 .NET toolkit containing multiple extensions, utilities and components to make it easier to work with PostgreSQL.
 
 Includes a channel based bulk importer for efficient bulk inserts using the COPY methods from Npgsql.
 
-To be continued...
+
+## Contributors
+
+![GitHub Contributors Image](https://contrib.rocks/image?repo=RagingKore/resistance.postgres)
+
+Made with [contributors-img](https://contrib.rocks).
+
+## Acknowledgements
+
+* [PostgreSQLCopyHelper](https://github.com/PostgreSQLCopyHelper/PostgreSQLCopyHelper)
+* [Npgsql - the .NET data provider for PostgreSQL](https://github.com/npgsql/npgsql)
--- src/Resistance.Postgres/Import/Metrics/BulkImporterMetrics.cs
+++ src/Resistance.Postgres/Import/Metrics/BulkImporterMetrics.cs
@@ -8,21 +8,14 @@ namespace Resistance.Postgres.Metrics
 
     static class BulkImporterMetrics
     {
-        static ICountMetric SuccessfulImportsCount { get; } = Instance.CreateCount("preem_postgres_imports_successful", "", "table", "correlation_id", "row_count");
-
-        static IHistogramMetric SuccessfulImportsDuration { get; } = Instance.CreateHistogram("preem_postgres_imports_successful_duration", "", "table", "correlation_id", "row_count");
-
-        static ICountMetric FailedImportsCount { get; } = Instance.CreateCount("preem_postgres_imports_failed", "", "table", "correlation_id");
-
-        static IHistogramMetric FailedImportsDuration { get; } = Instance.CreateHistogram("preem_postgres_imports_failed_duration", "", "table", "correlation_id");
-
-        static ICountMetric SuccessfulBatchesCount { get; } = Instance.CreateCount("preem_postgres_imports_batches_successful", "", "table", "correlation_id", "batch_id", "batch_size");
-
-        static IHistogramMetric SuccessfulBatchesDuration { get; } = Instance.CreateHistogram("preem_postgres_imports_batches_successful_duration", "", "table", "correlation_id", "batch_id", "batch_size");
-
-        static ICountMetric FailedBatchesCount { get; } = Instance.CreateCount("preem_postgres_imports_batches_failed", "", "table", "correlation_id", "batch_id", "batch_size");
-
-        static IHistogramMetric FailedBatchesDuration { get; } = Instance.CreateHistogram("preem_postgres_imports_batches_failed_duration", "", "table", "correlation_id", "batch_id", "batch_size");
+        static ICountMetric     SuccessfulImportsCount    { get; } = Instance.CreateCount("resistance_postgres_imports_successful", "", "table", "correlation_id", "row_count");
+        static IHistogramMetric SuccessfulImportsDuration { get; } = Instance.CreateHistogram("resistance_postgres_imports_successful_duration", "", "table", "correlation_id", "row_count");
+        static ICountMetric     FailedImportsCount        { get; } = Instance.CreateCount("resistance_postgres_imports_failed", "", "table", "correlation_id");
+        static IHistogramMetric FailedImportsDuration     { get; } = Instance.CreateHistogram("resistance_postgres_imports_failed_duration", "", "table", "correlation_id");
+        static ICountMetric     SuccessfulBatchesCount    { get; } = Instance.CreateCount("resistance_postgres_imports_batches_successful", "", "table", "correlation_id", "batch_id", "batch_size");
+        static IHistogramMetric SuccessfulBatchesDuration { get; } = Instance.CreateHistogram("resistance_postgres_imports_batches_successful_duration", "", "table", "correlation_id", "batch_id", "batch_size");
+        static ICountMetric     FailedBatchesCount        { get; } = Instance.CreateCount("resistance_postgres_imports_batches_failed", "", "table", "correlation_id", "batch_id", "batch_size");
+        static IHistogramMetric FailedBatchesDuration     { get; } = Instance.CreateHistogram("resistance_postgres_imports_batches_failed_duration", "", "table", "correlation_id", "batch_id", "batch_size");
 
         public static void TrackSuccessfulImport(string table, Guid correlationId, long rowCount, long startTime) {
             var labels = new[] {
